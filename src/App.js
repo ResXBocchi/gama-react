b5beb159158f8897679a027c364f2cb3bf5b2269
Renan Bocchi <67618681+ResXBocchi@users.noreply.github.com>
2020-11-24 22:45:27 -0300

    aprendendo estilizar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
+import React from 'react';
+import Routes from './routes';
+import './style.css';
 
 function App(props) {
-  const [ usuario, setUsuario ] = useState('');
 
-  function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
-  }
 
   return (
-    <>
-    <p> { usuario } </p>
-    <label name='usuario'>Usuário: </label>
-    <input name='usuario' id='usuario' className='usuarioInput' value={ usuario } placeholder='Usuário' onChange={e => setUsuario(e.target.value) }/>
-    <button type='button' onClick={handlePesquisa}>Pesquisar</button>
-    </>
+    <Routes/>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Repositories from './Repositories';
+import Repositories from './pages/Repositories';
+import Home from './pages/Home/index'
 
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
+
                 <Route path='/repositories' component={Repositories} />
+                <Route path='/' exact component={Home} />
             </Switch>
         </BrowserRouter>
     );
